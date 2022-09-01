768964ebb8ade0076a93a1258250390ded726368
fausio luis <fausioluis@live.com>
2022-09-01 10:35:09 +0200

    Add project files.

---
+++ Sha256/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sha256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes("girassol2022");
+                var hash = sha.ComputeHash(bytes);
+
+
+                var result =  Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
